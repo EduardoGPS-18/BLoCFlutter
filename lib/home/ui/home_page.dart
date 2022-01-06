b228f84e3874ee1507b7a51278bc47a8b64dcc4f
EduardoGPS-18 <eduardogps202@gmail.com>
2022-01-06 03:10:37 -0300

    feat: close keyboard on background click

--- lib/home/ui/home_page.dart
+++ lib/home/ui/home_page.dart
@@ -14,51 +14,57 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(24),
-        child: StreamBuilder<HomeState>(
-          initialData: presenter.currentHomeState,
-          stream: presenter.homeStateStream,
-          builder: (context, snapshot) {
-            final state = snapshot.data ?? presenter.currentHomeState;
-            switch (state.runtimeType) {
-              case HomeStateEnterForm:
-                final formState = state as HomeStateEnterForm;
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Padding(
+          padding: const EdgeInsets.all(24),
+          child: StreamBuilder<HomeState>(
+            initialData: presenter.currentHomeState,
+            stream: presenter.homeStateStream,
+            builder: (context, snapshot) {
+              final state = snapshot.data ?? presenter.currentHomeState;
+              switch (state.runtimeType) {
+                case HomeStateEnterForm:
+                  final formState = state as HomeStateEnterForm;
 
-                final hasEmailError = formState.emailError != ValidationError.noError;
-                final showEmailInputBorder = formState.emailError != ValidationError.requiredFieldWithoutBorder;
-                final showEmailError = hasEmailError && showEmailInputBorder;
-                final emailError = showEmailError ? formState.emailError.description : null;
+                  final hasEmailError = formState.emailError != ValidationError.noError;
+                  final showEmailInputBorder = formState.emailError != ValidationError.requiredFieldWithoutBorder;
+                  final showEmailError = hasEmailError && showEmailInputBorder;
+                  final emailError = showEmailError ? formState.emailError.description : null;
 
-                final hasPasswordError = formState.passwordError != ValidationError.noError;
-                final showPassInputBorder = formState.passwordError != ValidationError.requiredFieldWithoutBorder;
-                final showPasswordError = hasPasswordError && showPassInputBorder;
-                final passwordError = showPasswordError ? formState.passwordError.description : null;
+                  final hasPasswordError = formState.passwordError != ValidationError.noError;
+                  final showPassInputBorder = formState.passwordError != ValidationError.requiredFieldWithoutBorder;
+                  final showPasswordError = hasPasswordError && showPassInputBorder;
+                  final passwordError = showPasswordError ? formState.passwordError.description : null;
 
-                void submitFunction() => presenter.emmitEvent(HomeEventSubmit());
+                  void submitFunction() => presenter.emmitEvent(HomeEventSubmit());
 
-                return HomeFormWidget(
-                  emailError: emailError,
-                  validateEmail: (email) => presenter.emmitEvent(
-                    HomeValidateForm(fieldName: 'email', value: email),
-                  ),
-                  passwordError: passwordError,
-                  validatePassword: (password) => presenter.emmitEvent(
-                    HomeValidateForm(fieldName: 'password', value: password),
-                  ),
-                  onSubmit: (formState.isFormValid == true) ? submitFunction : null,
-                );
+                  return HomeFormWidget(
+                    emailError: emailError,
+                    validateEmail: (email) => presenter.emmitEvent(
+                      HomeValidateForm(fieldName: 'email', value: email),
+                    ),
+                    passwordError: passwordError,
+                    validatePassword: (password) => presenter.emmitEvent(
+                      HomeValidateForm(fieldName: 'password', value: password),
+                    ),
+                    onSubmit: (formState.isFormValid == true) ? submitFunction : null,
+                  );
 
-              case HomeStateLoading:
-                return const LoadingWidget();
+                case HomeStateLoading:
+                  return const LoadingWidget();
 
-              default:
-                return const Center(
-                  child: Text('Ocorreu um erro ao carregar o estado!'),
-                );
-            }
-          },
+                default:
+                  return const Center(
+                    child: Text('Ocorreu um erro ao carregar o estado!'),
+                  );
+              }
+            },
+          ),
         ),
       ),
     );
