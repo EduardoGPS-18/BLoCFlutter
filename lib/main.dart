fc65b34d3570f09e8b76dc2631751b7cc6e7b5cc
EduardoGPS-18 <eduardogps202@gmail.com>
2022-01-06 03:10:50 -0300

    feat: add theme

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,39 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        inputDecorationTheme: const InputDecorationTheme(
+          contentPadding: EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+          filled: true,
+          hoverColor: Colors.pink,
+          errorStyle: TextStyle(color: Colors.pink),
+          errorBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.pink, width: 1),
+            borderRadius: BorderRadius.all(Radius.circular(24)),
+            gapPadding: 12,
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.pink, width: 1),
+            borderRadius: BorderRadius.all(Radius.circular(24)),
+            gapPadding: 12,
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black, width: 1),
+            borderRadius: BorderRadius.all(Radius.circular(24)),
+            gapPadding: 12,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+              const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+            ),
+            shape: MaterialStateProperty.all<OutlinedBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24),
+              ),
+            ),
+          ),
+        ),
       ),
       home: HomePage(
         presenter: HomeBloc(),
